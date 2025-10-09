cbbd0672d3404a6b58921ab05b811e47794390b0
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-10-09 16:25:44 +0200

    test: add initial unit tests

---
+++ src/test/java/de/unimarburg/diz/mtbpidtokafka/utils/CustomKafkaKeyGeneratorTest.java
@@ -0,0 +1,23 @@
+package de.unimarburg.diz.mtbpidtokafka.utils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomKafkaKeyGeneratorTest {
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "H/2025/54321,01234567,H54321-25_PID01234567",
+            "X/2025/654321,01234567,H654321-25_PID01234567",
+            "H/1925/7654321,01234567,H7654321-25_PID01234567",
+            "H/2025/01234,12345678,H1234-25_PID12345678",
+            "H202554321,01234567,no journal or pid number present",
+    })
+    public void shouldGenerateCustomPatientIdentifier(String einsendenummer, String patientId, String expectedKey) {
+        assertThat(CustomKafkaKeyGenerator.generateCustomPatientIdentifier(einsendenummer, patientId)).isEqualTo(expectedKey);
+    }
+
+}
