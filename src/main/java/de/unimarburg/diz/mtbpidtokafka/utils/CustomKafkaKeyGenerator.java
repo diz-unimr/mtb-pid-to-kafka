29cc55da36f4ff5b1e4bed6bab1e697c3ddcf2f7
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-10-16 10:21:16 +0200

    feat: add alternative form of Einsendenummer

--- src/main/java/de/unimarburg/diz/mtbpidtokafka/utils/CustomKafkaKeyGenerator.java
+++ src/main/java/de/unimarburg/diz/mtbpidtokafka/utils/CustomKafkaKeyGenerator.java
@@ -4,31 +4,43 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CustomKafkaKeyGenerator {
+
     private static final Logger log = LoggerFactory.getLogger(CustomKafkaKeyGenerator.class);
+
     public static String generateCustomPatientIdentifier(String einsendenummer, String patientenId) {
 
         if (StringUtils.hasText(einsendenummer) && StringUtils.hasText(patientenId)) {
             log.debug("Einsendenummer is not null");
 
-            final var pattern = Pattern.compile("(?<prefix>[A-Z])/\\d{2}(?<year>\\d{2})/0*(?<number>\\d+)");
-            final var matcher = pattern.matcher(einsendenummer);
+            final var pattern1 = Pattern.compile("(?<prefix>[A-Z])/\\d{2}(?<year>\\d{2})/0*(?<number>\\d+)");
+            final var matcher1 = pattern1.matcher(einsendenummer);
 
-            if (matcher.find()) {
-                final var prefix = matcher.group("prefix");
-                final var year = matcher.group("year");
-                final var number = matcher.group("number");
+            final var pattern2 = Pattern.compile("(?<prefix>[A-Z])0*(?<number>\\d+)-(?<year>\\d{2})");
+            final var matcher2 = pattern2.matcher(einsendenummer);
 
-                if (!prefix.equals("H")) {
-                    log.error(String.format("The prefix must be 'H'. Provided: %s", prefix));
-                }
-                
-                return String.format("%s%s-%s_PID%s", "H", number, year, patientenId);
+            if (matcher1.find()) {
+                return keyFromMatcher(matcher1, patientenId);
+            } else if (matcher2.find()) {
+                return keyFromMatcher(matcher2, patientenId);
             }
             log.error("The einsendennummer is not valid");
         }
         return "no journal or pid number present";
+    }
+
+    private static String keyFromMatcher(Matcher matcher, String patientenId) {
+        final var prefix = matcher.group("prefix");
+        final var year = matcher.group("year");
+        final var number = matcher.group("number");
+
+        if (!prefix.equals("H")) {
+            log.error(String.format("The prefix must be 'H'. Provided: %s", prefix));
+        }
+
+        return String.format("%s%s-%s_PID%s", "H", number, year, patientenId);
     }
 }
--- src/test/java/de/unimarburg/diz/mtbpidtokafka/utils/CustomKafkaKeyGeneratorTest.java
+++ src/test/java/de/unimarburg/diz/mtbpidtokafka/utils/CustomKafkaKeyGeneratorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CustomKafkaKeyGeneratorTest {
+class CustomKafkaKeyGeneratorTest {
 
 
     @ParameterizedTest
@@ -14,9 +14,13 @@ public class CustomKafkaKeyGeneratorTest {
             "X/2025/654321,01234567,H654321-25_PID01234567",
             "H/1925/7654321,01234567,H7654321-25_PID01234567",
             "H/2025/01234,12345678,H1234-25_PID12345678",
+            "H54321-25,01234567,H54321-25_PID01234567",
+            "X654321-25,01234567,H654321-25_PID01234567",
+            "H7654321-25,01234567,H7654321-25_PID01234567",
+            "H01234-25,12345678,H1234-25_PID12345678",
             "H202554321,01234567,no journal or pid number present",
     })
-    public void shouldGenerateCustomPatientIdentifier(String einsendenummer, String patientId, String expectedKey) {
+    void shouldGenerateCustomPatientIdentifier(String einsendenummer, String patientId, String expectedKey) {
         assertThat(CustomKafkaKeyGenerator.generateCustomPatientIdentifier(einsendenummer, patientId)).isEqualTo(expectedKey);
     }
 
