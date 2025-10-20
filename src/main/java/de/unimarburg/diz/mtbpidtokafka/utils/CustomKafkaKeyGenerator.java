f222694994cc4e569373f090be48e26a33c375af
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-10-20 15:27:18 +0200

    refactor: kafka key generation (#29)
    * test: add initial unit tests
    
    * refactor: kafka key generation
    
    * feat: add alternative form of Einsendenummer

--- src/main/java/de/unimarburg/diz/mtbpidtokafka/utils/CustomKafkaKeyGenerator.java
+++ src/main/java/de/unimarburg/diz/mtbpidtokafka/utils/CustomKafkaKeyGenerator.java
@@ -4,41 +4,43 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CustomKafkaKeyGenerator {
+
     private static final Logger log = LoggerFactory.getLogger(CustomKafkaKeyGenerator.class);
+
     public static String generateCustomPatientIdentifier(String einsendenummer, String patientenId) {
 
         if (StringUtils.hasText(einsendenummer) && StringUtils.hasText(patientenId)) {
             log.debug("Einsendenummer is not null");
 
-            // Split the einsendenummer by '/'
-            if (einsendenummer.contains("/")) {
-                String[] parts = einsendenummer.split("/");
+            final var pattern1 = Pattern.compile("(?<prefix>[A-Z])/\\d{2}(?<year>\\d{2})/0*(?<number>\\d+)");
+            final var matcher1 = pattern1.matcher(einsendenummer);
 
-                // Check if the prefix is "H"
-                String prefix = parts[0]; // Assuming the first part is the prefix
-                if (!prefix.endsWith("H")) {
-                    log.error("The prefix must be 'H'. Provided: " + prefix);
-                }
+            final var pattern2 = Pattern.compile("(?<prefix>[A-Z])0*(?<number>\\d+)-(?<year>\\d{2})");
+            final var matcher2 = pattern2.matcher(einsendenummer);
 
-
-                String secondPart = parts[parts.length - 2];
-                // Extract the last two characters after the leading "0" from the second part
-                String secondNumber = secondPart.length() > 1 ? secondPart.substring(2) : "0";
-
-                // Split the last part
-                // Extract the required parts
-                String lastPart = parts[parts.length - 1]; // Last part
-                // Convert to unsigned integers
-                String [] lastParts = lastPart.split("\\.");
-                String lastPartStart = lastParts[0];
-                int firstNumber = Integer.parseInt(lastPartStart);
-
-                // Construct the custom patient identifier
-                return "H" + firstNumber + "-" + secondNumber + "_PID" + patientenId;
+            if (matcher1.find()) {
+                return keyFromMatcher(matcher1, patientenId);
+            } else if (matcher2.find()) {
+                return keyFromMatcher(matcher2, patientenId);
             }
             log.error("The einsendennummer is not valid");
         }
         return "no journal or pid number present";
+    }
+
+    private static String keyFromMatcher(Matcher matcher, String patientenId) {
+        final var prefix = matcher.group("prefix");
+        final var year = matcher.group("year");
+        final var number = matcher.group("number");
+
+        if (!prefix.equals("H")) {
+            log.error(String.format("The prefix must be 'H'. Provided: %s", prefix));
+        }
+
+        return String.format("%s%s-%s_PID%s", "H", number, year, patientenId);
     }
 }
